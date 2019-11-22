1fdda291400953b404c4a649fdc9b47521f32aeb
fd220 <55639368+fd220@users.noreply.github.com>
2019-11-22 14:53:32 +0100

    Update Array_read.cc

--- Array_read.cc
+++ Array_read.cc
@@ -1,19 +1,22 @@
 #include "fcpp.hh"
 
 
-int array_output
-int array_sort
-int return_break
+int array_output (int[] array) {
+	cout<<"Array: [ ";
+	for (int i=0; i<10; i++) {
+		cout<<array[i]<<" ";
+	};
+	cout<<"]"<<endl;
+}
 
 
-int input_case (int input, int array) {
-	
-	switch (input) {
-		case -1: cout<<"Programm terminiert!"; return -1; break;
-		case 0: array_output(array); return 0; break;
-		default: return input; break;
-	}
-} 
+int return_break () {
+	int end;
+	cout<<"Das Array ist voll!"<<endl;
+	cin>>end;
+	if (end!=-1) {cout "ARRAY IST VOLL"; return -1;}
+	return 0;
+}
 
 
 int main () {
@@ -23,13 +26,20 @@ int main () {
 	int input;
 	
 	while (i<10) {
+		cout<<"Geben Sie eine Zahl ein: ";
 		cin>>input;
-		array [i] = input_case (input, array);
-		array_sort(array);
 		
-		if (array[i]== -1) {i=10;};
-		if (array[i]!=0) { i++;};
+		if (input<-1) {cout<<"Nur positive Zahlen erlaubt!"; i=100;};
+			else if (input==-1) {cout<<"Programm terminiert!"; i=100;};
+			else if (input==0) {array_output(array);};
+			else {array[i]=input; i++;};
 	};
+	
+	for (int m=i; i>=0, m--) {
+		for (int k=0; k<=m; k++) {
+			if (array[k]<array[k+1]) {int zwischenspeicher=array[k]; array[k]=array[k+1]; array[k+1]=zwischenspeicher;}
+		}
+	}
 	
 	return return_break;
 }
