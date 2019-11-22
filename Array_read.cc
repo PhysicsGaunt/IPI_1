ab6c0823938aa1190c0e3a4f6e1365b7f80ff1d0
fd220 <55639368+fd220@users.noreply.github.com>
2019-11-22 15:03:29 +0100

    Update Array_read.cc

--- Array_read.cc
+++ Array_read.cc
@@ -10,36 +10,30 @@ int array_output (int[] array) {
 }
 
 
-int return_break () {
-	int end;
-	cout<<"Das Array ist voll!"<<endl;
-	cin>>end;
-	if (end!=-1) {cout "ARRAY IST VOLL"; return -1;}
-	return 0;
-}
-
-
 int main () {
 	
 	int array[10];
 	int i = 0;
 	int input;
 	
-	while (i<10) {
-		cout<<"Geben Sie eine Zahl ein: ";
-		cin>>input;
-		
-		if (input<-1) {cout<<"Nur positive Zahlen erlaubt!"; i=100;};
-			else if (input==-1) {cout<<"Programm terminiert!"; i=100;};
-			else if (input==0) {array_output(array);};
-			else {array[i]=input; i++;};
-	};
+	cout<<"Geben Sie eine Zahl ein: ";
+	cin>>input;
 	
-	for (int m=i; i>=0, m--) {
-		for (int k=0; k<=m; k++) {
-			if (array[k]<array[k+1]) {int zwischenspeicher=array[k]; array[k]=array[k+1]; array[k+1]=zwischenspeicher;}
+	do {
+		if (i<10) {
+			if (input<-1) {cout<<"Nur positive Zahlen erlaubt!"; i=-100;};
+				else if (input==-1) {cout<<"Programm terminiert!"; i=-100;};
+				else if (input==0) {array_output(array);};
+				else {array[i]=input; i++;};
+			else if (i==10) {cout<<"Array ist voll!"}
+
+
+		for (int m=i; i>=0, m--) {
+			for (int k=0; k<=m; k++) {
+				if (array[k]<array[k+1]) {int zwischenspeicher=array[k]; array[k]=array[k+1]; array[k+1]=zwischenspeicher;}
+			}	
 		}
-	}
+	} while (i!=-100);
 	
-	return return_break;
+	return 0;
 }
