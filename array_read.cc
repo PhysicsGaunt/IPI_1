e9dd01dac00441be643c7e2094471ba6af425d6c
fd220 <55639368+fd220@users.noreply.github.com>
2019-11-28 01:18:26 +0100

    Add files via upload

---
+++ array_read.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+/* Da keine Arrays als Argumente oder returns ist alles in der main function.
+   input: Werte des Arrays bzws. 0 (Array ausgeben), -1 (Programm terminieren);
+   return 0*/ 
+
+int main () {
+	
+	int array[10];			// Array wird definiert
+	int i = 0;
+	int input;
+	
+	for (int k=0; k<10; k++) array[k]=0;		// Array wird in jeder Stelle auf 0 gesetzt
+	
+	do {												// do...while schleife sodass bei Fehlern oder Eingabe "-1" terminiert werden kann
+	cout<<"Geben Sie die "<<i+1<<". Zahl ein: ";
+	cin>>input;												// Eingabe Wert fuer Array
+	
+		if (input<-1) {cout<<"Nur positive Zahlen erlaubt\n!"; i=-100;}	// Abfangen negativer Eingaben außer "-1"
+		
+		else if (input==-1) {cout<<"Programm terminiert!\n"; i=-100;}		// "-1" terminiert
+		
+		else if (input==0) {											// "0" gibt aktuelles Array aus
+				cout<<"Array: [ ";
+				for (int i=0; i<10; i++) {
+					cout<<array[i]<<" ";
+				}
+				cout<<"]"<<endl;
+				}
+				
+		else  if (i<10) {array[i]=input; i++;}							// Normale Eingabe ausser bei vollem Array
+			else {cout<<"ARRAY IST VOLL!\n"; i=-100; }
+	
+		if (i==10) {cout<<"Array ist voll!\n";}							// Letzte moegliche Eingabe
+
+
+		for (int m=0; m<=i; m++) {										// bubble sort
+			for (int l=0; l<i-m-1; l++) {
+				if (array[l]>array[l+1]) {int zwischenspeicher=array[l]; array[l]=array[l+1]; array[l+1]=zwischenspeicher;}
+			}	
+		}
+	} while(i!=-100);
+	
+	return 0;
+}
+
--- shuffle.cc
+++ shuffle.cc
@@ -4,17 +4,6 @@
 
 using namespace std;
 
-enum shuffle_mode {IN=1, OUT=2}; 		// definiere shuffle_mode als enum fuer Mischart
-
-istream& operator>> (istream& in, shuffle_mode& shuffle) {		// overload Eingabeoperator, damit ein cin eines strings zu einer shuffle_mode transformiert wird bei cin>>shuffle
-    std::string token;
-    in >> token;
-    if ( token == "IN" || token == "1" ){shuffle=IN;}
-    else if ( token == "OUT" || token == "2" ){shuffle=OUT;}
-    else { cout<<"Nur 'IN' oder 'OUT' sind akzeptierte Eingaben!" ;}
-    return in;
-}
-
 /* function deck_check: kontrolliert ob Karten in ursruenglicher Reihenfolge;
 	input deck: aktualle Kartenposition, n: Kartenanzahl
 	return true falls alle urspruengliche Reihenfolge sonst false*/
@@ -28,61 +17,56 @@ bool deck_check (int deck[], int n) {
 	return true;
 }
 
-/* function main: Deklarierungen und "mischen" der Karten
-	input: shuffle: Mischmethode (IN / OUT) 
-	return 0*/
-
+/*function main: fuehrt den Algorithmus zum perfect suffle fuer n Karten aus und gibt das Ergebniss aus.
+	input: #Karten n im Code, da Arrays nicht dynamischen Platz reservieren duerfen
+	output: return 0; Anzahl Mischen zur urspruenglichen Reihenfolge als cout */
 int main () {
-	
-	
-	shuffle_mode shuffle;     
 	
 	int n=200;							// n: Kartenanzahl: muss gerade sein
 	int deck[n];
 	int deck_1[n/2];					// erste haelfte der Karten
 	int deck_2[n/2];					// zweite haelfte der Karten
-	int iterations=0;					// Anzahl Vertauschungen
+	int iterations_in=0;
+	int iterations_out=0;					// Anzahl Vertauschungen
 	int position_hand_1=0, position_hand_2=0;		// Zeigt auf Positionen in den Kartenhaelften
 	
 	for (int i=0; i<n; i++) { deck[i]=i; }				// Deck definiert als (0,...,n-1)
 	
-	cout<<"Shuffle mode? (IN or OUT): ";
-	cin>>shuffle;
+
+	do {						// Perfect shuffle in
+		position_hand_1=0; position_hand_2=0; // reset hand position
+		
+		for (int k=0; k<n/2; k++) { deck_1[k]=deck[k]; }					// Aufteilen der Karten
+		for (int k=0; k<n/2; k++) { deck_2[k]=deck[k+n/2]; }
+		
+		for (int k=0; k<n; k++) {
+			if (k%2==0) { deck[k]=deck_1[position_hand_1]; position_hand_1++;}  // Karten neu "zusammenshufflen"
+			else { deck[k]=deck_2[position_hand_2]; position_hand_2++;}
+		}
+			
+		iterations_in++;
+			
+	} while (deck_check(deck, n)!=true);									// ueberpfuefung urspruengliche reihenfolge
+
+
+								// perfect shuffle out
+	do {
+		position_hand_1=0; position_hand_2=0;	//reset hand position
+		
+		for (int k=0; k<n/2; k++) { deck_1[k]=deck[k]; }					// Aufteilen der Karten
+		for (int k=0; k<n/2; k++) { deck_2[k]=deck[k+n/2]; }
+		
+		for (int k=0; k<n; k++) {
+			if (k%2==0) { deck[k]=deck_2[position_hand_2]; position_hand_2++;}	// Karten neu "zusammenshufflen"
+			else { deck[k]=deck_1[position_hand_1]; position_hand_1++;}
+		}
+			
+		iterations_out++;
+		
+	} while (deck_check(deck, n)!=true);								// ueberpfuefung urspruengliche reihenfolge
+
 	
-	if (shuffle == IN) {             // Perfect-in-shuffle
-		do {
-			position_hand_1=0; position_hand_2=0; // reset hand position
-			
-			for (int k=0; k<n/2; k++) { deck_1[k]=deck[k]; }					// Aufteilen der Karten
-			for (int k=0; k<n/2; k++) { deck_2[k]=deck[k+n/2]; }
-			
-			for (int k=0; k<n; k++) {
-				if (k%2==0) { deck[k]=deck_1[position_hand_1]; position_hand_1++;}  // Karten neu "zusammenshufflen"
-				else { deck[k]=deck_2[position_hand_2]; position_hand_2++;}
-			}
-			
-			iterations++;
-			
-		} while (deck_check(deck, n)!=true);									// ueberpfuefung urspruengliche reihenfolge
-	}
-	
-	if (shuffle == OUT) {			// Perfect-out-shuffle
-		do {
-			position_hand_1=0; position_hand_2=0;	//reset hand position
-			
-			for (int k=0; k<n/2; k++) { deck_1[k]=deck[k]; }					// Aufteilen der Karten
-			for (int k=0; k<n/2; k++) { deck_2[k]=deck[k+n/2]; }
-			
-			for (int k=0; k<n; k++) {
-				if (k%2==0) { deck[k]=deck_2[position_hand_2]; position_hand_2++;}	// Karten neu "zusammenshufflen"
-				else { deck[k]=deck_1[position_hand_1]; position_hand_1++;}
-			}
-				
-			iterations++;
-			
-		} while (deck_check(deck, n)!=true);								// ueberpfuefung urspruengliche reihenfolge
-	}
-	
-	cout<<"\nNach "<<iterations<<" mal mischen sind die Karten wieder identisch zum Anfang.";
+	cout<<"\nNach "<<iterations_in<<" mal mischen sind die Karten wieder identisch zum Anfang mit perfect-shuffle-in.";
+	cout<<"\nNach "<<iterations_out<<" mal mischen sind die karten wieder identisch zum Anfang mit perfect-shuffle-out.";
 	return 0;
 }
