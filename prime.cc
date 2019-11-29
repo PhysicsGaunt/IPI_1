8f1d43c659b4742e1763e19cfa214bd3156a8911
fd220 <55639368+fd220@users.noreply.github.com>
2019-11-29 17:55:03 +0100

    Update prime.cc

--- prime.cc
+++ prime.cc
@@ -4,24 +4,38 @@
 
 struct primes {
 	int prime;
-}
+	int position=0;
+	int prime[2][500];
+};
 
-int next_divident (int prime, int progress) {
+int next_divident (int progress, int position) {
 	do {
-		if (prime%progress==0) {primes[progress]=1; return progress;}
-	} while(progress<=sqrt(prime);
+		if (prime%progress==0) {primes[1][position]=1; return progress;}
+		else {prime++;}
+	} while(progress<=sqrt(prime));
 	return 0;
 }
 
-void potenz (int prime, int divident, int divident count) {
-	int i=2;
+void potenz (int progress, int position) {
+	int count=2;
 	do {
-		if (prime%(i*divident)==0) {potenz[count]++;}
-		else {i=prime;)
-		i++;
-	} while (i<=sqrt(prime))
+		if (prime%(count*progress)==0) {primes[1][position-1]++;}
+		else {count=prime;}
+		count++;
+	} while (count<=sqrt(prime))
 }
 
 int main (int argc, char** argv) {
-	do while 
+	prime p;
+	p.prime = argv[0];
+	int progress=2, position=0;
+	
+	do {
+		progress=next_divident(progress,position);
+		if (progress!=0) {
+			potenz(progress, position);
+		}
+		progress++; position++;
+			
+	} while(progress<=sqrt(prime));
 	 	
