b09aed39ff45dc8190c17d2f37af034903c873cc
fd220 <55639368+fd220@users.noreply.github.com>
2019-11-29 17:33:13 +0100

    Create prime.cc

---
+++ prime.cc
@@ -0,0 +1,27 @@
+#include <iosteam>
+#include <stdlib.h>
+#include <math.h>
+
+struct primes {
+	int prime;
+}
+
+int next_divident (int prime, int progress) {
+	do {
+		if (prime%progress==0) {primes[progress]=1; return progress;}
+	} while(progress<=sqrt(prime);
+	return 0;
+}
+
+void potenz (int prime, int divident, int divident count) {
+	int i=2;
+	do {
+		if (prime%(i*divident)==0) {potenz[count]++;}
+		else {i=prime;)
+		i++;
+	} while (i<=sqrt(prime))
+}
+
+int main (int argc, char** argv) {
+	do while 
+	 	
