c9fd459f700f78a1777da049bafad491af525bde
fd220 <55639368+fd220@users.noreply.github.com>
2019-12-05 00:52:08 +0100

    Update prime.cc

--- prime.cc
+++ prime.cc
@@ -11,12 +11,13 @@ int main() {
 	cout<<"Zahl: ";
 	cin>>zahl;
 	cout<<"Primzahlzerlegung von "<<zahl<<" : ";
-	for (teiler=2; teiler<=sqrt(zahl)+1; teiler++)
-		while (zahl%teiler==0) {
-			if(erstes==true)	cout << teiler;
-			else				cout << "*" << teiler;
+	for (teiler=2; teiler<=sqrt(zahl)+1; teiler++)				// Alle Teiler
+		while (zahl%teiler==0) {					// immer noch teilbar
+			if(erstes==true)	cout << teiler;			// Ausgabe
+			else			cout << "*" << teiler;	
 			erstes=false;
-			zahl/=teiler;
+			zahl/=teiler;						// Primzahl aus der Zahl herausnehmen
 		}
-		return 0;
+	if (erstes==true) cout<<zahl;						// Falls Primzahl
+	return 0;
 }
